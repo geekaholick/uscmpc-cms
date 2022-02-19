c6bab519c66458dde89d4cbc0ec5d2ffca4b93fe
Jan_Patrick Tancinco <janpatricktancinco@gmail.com>
2022-02-19 15:47:08 +0800

    T-04: remove image request file

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,21 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Request\ImageRequest;
 
-// class ImageController extends Controller
 class ImageController
 {
-    /**
-     * Not yet needed
-     * @param Request $request
-     * @return string $imageName
-     */
-    public function storeUpdateImage(ImageRequest $request)
-    {
-        // code ...
-    }
-
     /**
      * Save the image in the public path and returns the image name
      * @param Request $request
